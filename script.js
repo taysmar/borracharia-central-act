d6f897660254eac7dfd73e705da775201a58c674
Taysmar <taysmar@outlook.com>
2022-08-27 12:15:51 -0300

    Finalização dos  ultimos detalhes. Layout finalizado

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ hamburger.addEventListener('click', (event) => {
     linksNavbar.classList.toggle('show');
     console.log(linksNavbar);
 })
+
+var typed = new Typed(".hero-text-unstatic", {
+    strings: ["HISTÓRIA", "QUALIDADE", "TRADIÇÃO"],
+    typeSpeed: 200,
+    loop: true,
+    backDelay: 900,
+    backSpeed: 60,
+});
